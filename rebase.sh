8449c0da885cfdce618e7f2bf5a14c0a1fd5a67b
Nathaniel Kaiser <kaisernathaniel@gmail.com>
2025-08-09 12:29:48 -0500

    remove rebase script

--- rebase.sh
+++
@@ -1,6 +0,0 @@
-# Set your new name and email
-NEW_NAME="Nathaniel Kaiser"
-NEW_EMAIL="kaisernathaniel@gmail.com"
-
-# Start an interactive rebase with autosquash disabled
-git rebase -i --exec "git commit --amend --no-edit --author=\"$NEW_NAME <$NEW_EMAIL>\"" HEAD~7
