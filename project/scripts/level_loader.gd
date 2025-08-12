0c05376c8a6e8e615686aafcf3ba45354911f495
Nathaniel Kaiser <kaisernathaniel@gmail.com>
2025-08-12 13:43:36 -0500

    level timer

--- project/scripts/level_loader.gd
+++ project/scripts/level_loader.gd
@@ -17,13 +17,26 @@ var level_color = [
 	Color(0.0, 1.0, 1.0),
 	Color(0.0, 1.0, 0.0),
 ]
+var level_times = [
+	0,
+	0,
+	0,
+	0
+]
+
 
 var current_level_index := 0
+var current_level_time_index := 0
 
+
+
+func _physics_process(delta: float) -> void:
+	level_times[current_level_time_index] += delta
 
 
 func start_game():
 	current_level_index = 0
+	current_level_time_index = 1
 	load_level(current_level_index)
 
 func load_level(index: int):
@@ -39,6 +52,7 @@ func load_level(index: int):
 		get_tree().change_scene_to_file("res://WinScreen.tscn")
 
 func next_level():
+	print("time to complete level:", level_times[current_level_time_index] )
 	load_level(current_level_index + 1)
 	
 func get_level_color():
