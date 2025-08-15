2086eea6226adb0ebed78626a96565260004317d
Nathaniel Kaiser <kaisernathaniel@gmail.com>
2025-08-15 10:32:48 -0500

    jump splash fix

--- project/jump_splash.gd
+++ project/jump_splash.gd
@@ -3,7 +3,6 @@ extends AnimatedSprite2D
 func _ready():
 	animation = "default"
 	play()
-	connect("animation_finished", Callable(self, "_on_animation_finished"))
 
 func _on_animation_finished() -> void:
 	queue_free()
